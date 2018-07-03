9ffb706761b57ab489d91d4e4149e9311104c24b
xinyuexy <1151980485@qq.com>
2018-07-03 21:13:45 +0800

    Completed lab2

---
+++ lab2setup/DogTest.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class DogTest {    
+    @Test
+    public void testSmall() {
+        Dog d = new Dog(3);
+        assertEquals("yip", d.noise());
+    }
+
+    @Test
+    public void testLarge() {
+        Dog d = new Dog(20);
+        assertEquals("bark", d.noise());
+    }
+}
